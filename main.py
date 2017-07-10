985706ce1e59049c37404462a7815bb28f5fb302
fidasx <fidasx@gmail.com>
2017-07-10 16:43:40 +0300

    main
    main.py

---
+++ main.py
@@ -0,0 +1,69 @@
+import time
+import os
+from datetime import datetime
+import requests
+from termcolor import colored
+
+
+os.system("cls")
+
+
+
+def result():
+    
+    url = 'https://dgb-groestl.theblocksfactory.com/api.php?api_key='
+    r = requests.get(url+apikey)
+
+    data = r.json()
+    #time.sleep(0.1)
+
+    red("---------------------------------------WORKERS-----------------------------------------------")
+
+    for key, val in data["workers"].items():
+        timestamp = datetime.fromtimestamp(int(val["last_share_timestamp"])).strftime('%Y-%m-%d %H:%M:%S')
+        result.worker = print(key.upper() + " |HASHRATE| " + colored(float(val["hashrate"])/1000, "green") +" |LAST SHARE| "+ colored(timestamp, 'green'))
+
+    result.username = "USER: " + colored(data["username"], "green")
+    result.confirmed = "CONFIRMED: " + colored(round(float(data["confirmed_rewards"])), "green")
+    result.total = "TOTAL EARNED: " + colored(round(float(data["payout_history"])), "green")
+    result.round_shares = "ROUND SHARES: " + colored(int(data["round_shares"]), "green")
+    result.hashrate = "HASHRATE: " + colored(int(data["total_hashrate"])/1000, "green")
+
+    red("---------------------------------------------------------------------------------------------")
+    print("")
+
+def red(text):
+    return print(colored(text, "red"))
+
+
+def printstats():
+    red("----------------------------------------STATUS-----------------------------------------------")
+    print(result.username)
+    print(result.confirmed)
+    print(result.hashrate)
+    print(result.total)
+    print(result.round_shares)
+    red("---------------------------------------------------------------------------------------------")
+
+print(" please insert your api key from https://dgb-groestl.theblocksfactory.com/accountdetails \n" + "APIKEY:")
+apikey = input()
+
+if len(apikey) == 64:
+    userkey = apikey
+    os.system("cls")
+else:
+    print("wrong key")
+    quit()
+
+
+def update():
+
+    while True:
+        result()
+        printstats()
+        time.sleep(15)
+        os.system("cls")
+
+update()
+
+
