11befb17954375d8fbfda39e7c19718677815d8e
fidasx <fidasx@gmail.com>
2017-07-12 22:01:59 +0300

    input  rework

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ os.system("cls")
 
 def result():
     
-    url = 'https://dgb-'+useralgo+'.theblocksfactory.com/api.php?api_key='
-    r = requests.get(url+apikey)
+    url = 'https://dgb-'+userinput.algo+'.theblocksfactory.com/api.php?api_key='
+    r = requests.get(url+userinput.key)
 
     data = r.json()
     #time.sleep(0.1)
@@ -45,17 +45,36 @@ def printstats():
     print(result.round_shares)
     red("---------------------------------------------------------------------------------------------")
 
-print(" please insert your api key from https://dgb-groestl.theblocksfactory.com/accountdetails \n" + "APIKEY:")
-apikey = input()
-print("algorithm? example skein, groestl, qubit   etc...")
-algo = input()
-if len(apikey) == 64:
-    useralgo = algo
-    userkey = apikey
-    os.system("cls")
-else:
-    print("wrong key")
-    quit()
+
+
+def userinput():
+    
+    print(" please insert your api key from https://dgb-groestl.theblocksfactory.com/accountdetails \n" + "APIKEY:")
+    userinput.key = input()
+    
+    if len(userinput.key) == 64:
+        pass
+    else:
+        print("wrong key")
+        quit()
+
+    print("algorithm? example skein, groestl, qubit   etc...\n" +"ALGO:")
+    userinput.algo = input() 
+
+    algos=["skein", "groestl", "qubit"]
+        
+    if userinput.algo in algos:
+        os.system("cls")
+    else:
+        print("wrong algo")
+        quit()
+       
+
+    
+
+userinput()
+
+
 
 
 def update():
